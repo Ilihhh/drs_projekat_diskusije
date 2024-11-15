1db0024445ffc33414fce6e5a68e2cdf3936de65
Ilihhh <ilija.brdaric020@gmail.com>
2024-11-15 14:57:03 +0100

    Uputstvo za bazu u config.py

--- back/app/config.py
+++ back/app/config.py
@@ -7,3 +7,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")  # URI za pristup bazi
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
+
+
+# Za povezivanje sa bazom napravi .env i dodaj (sa parametrima koji pasu):
+# DATABASE_URI=postgresql://username:password@localhost:5432/ime_baze
+# SECRET_KEY=neki_tajni_kljuc
