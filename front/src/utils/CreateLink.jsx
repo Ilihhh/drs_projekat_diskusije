3850c9aaa05d490c08057375ffe2c35ea5cf04c7
Srky <p.srdjan02@gmail.com>
2024-11-22 17:07:28 +0100

    Small aesthetic fixes

--- front/src/utils/CreateLink.jsx
+++ front/src/utils/CreateLink.jsx
@@ -1,25 +1,32 @@
 import { Link } from "react-router-dom";
 
-
 export default function CreateLink(props) {
-    return (
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "1rem", marginTop: "2rem" }}>
-        <h2>
-          <Link
-            to={props.to}
-            style={{ 
-              textDecoration: "none", 
-              color: "gray", 
-              fontWeight: "bold", 
-              cursor: "pointer",
-              transition: "color 0.3s ease",
-            }}
-            onMouseEnter={(e) => e.target.style.color = "darkgray"}
-            onMouseLeave={(e) => e.target.style.color = "gray"}
-          >
-            {props.children}
-          </Link>
-        </h2>
-      </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: "1rem",
+        marginTop: "2rem",
+      }}
+    >
+      <h2>
+        <Link
+          to={props.to}
+          style={{
+            textDecoration: "none",
+            color: "black",
+            fontWeight: "bold",
+            cursor: "pointer",
+            transition: "color 0.3s ease",
+          }}
+          onMouseEnter={(e) => (e.target.style.color = "white")}
+          onMouseLeave={(e) => (e.target.style.color = "black")}
+        >
+          {props.children}
+        </Link>
+      </h2>
+    </div>
+  );
 }
