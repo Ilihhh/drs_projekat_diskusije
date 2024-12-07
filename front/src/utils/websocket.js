845318cc066dcd8b2f6665b8ee8f68dfd45d1837
Milan Petrović <milan.petrovic853@gmail.com>
2024-12-07 17:12:12 +0100

    v7

--- front/src/utils/websocket.js
+++ front/src/utils/websocket.js
@@ -1,7 +1,9 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://drs-projekat-diskusije.onrender.com"); // URL backend servera
+const socket = io("https://drs-projekat-diskusije.onrender.com", {
+  transports: ["websocket"],
+});
 
 socket.on("connect", () => {
   console.log("Connected to server");
